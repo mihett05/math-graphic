a9f422e8f5560e0b4377069630e2bf6d4ff74314
mihett05 <mihett05@yandex.ru>
2019-03-05 16:46:59 +0500

    Add files via upload

--- code.py
+++ code.py
@@ -2,6 +2,7 @@ from tkinter import *
 from numpy import arange
 from collections import namedtuple
 from math import *
+from PIL import Image, ImageDraw
 
 root = Tk()  # initialization
 root.resizable(width=False, height=False)  # turn off window's resizable
@@ -43,6 +44,45 @@ def build(event):
                           width=2, tag="line")
 
 
+def export(event):
+    global table
+    text = func.get("1.0", END)
+    text = text.replace("^", "**").replace(",", ".")
+    tasks = text.split(";")
+    y = lambda x, code: eval(code) * -1
+    im = Image.new("RGB", (width, height), (255, 255, 255))  # (220, 220, 220))
+    dr = ImageDraw.Draw(im)
+    for i in range(int(width / unit)):  # draws background cell
+        dr.line([i * unit, 0, i * unit, height], fill=(220, 220, 220))
+        dr.line([0, i * unit, width, i * unit], fill=(220, 220, 220))
+        if i == 15:
+            dr.text((i * unit - 10, height / 2 + 10), text=str(counter[i]), fill=string.color)
+        else:
+            dr.text((i * unit, height / 2 + 10), text=str(counter[i]), fill=(105, 105, 105))  # y
+            dr.text((width / 2 - 20, i * unit), text=str(r_counter[i]), fill=(105, 105, 105))  # x
+    graphic_bg = [(center, (width, height / 2)), (center, (width / 2, 0)), (center, (0, height / 2)),
+                  (center, (width / 2, height))]  # values for graphic background
+    for point in graphic_bg:
+        dr.line(point, width=2, fill=(0, 0, 0))  # draws graphic background
+    for task in tasks:
+        if "**" not in task and "/" not in task and "cos" not in task and "sin" not in task and "tan"\
+                not in task and "sqrt" not in task:
+            table = [(15, y(15, task)), (-15, y(-15, task))]  # make new values's tables
+            dr.line([center[0] + table[0][0] * unit, center[1] + table[0][1] * unit,
+                     center[0] + table[1][0] * unit, center[1] + table[1][1] * unit],
+                    fill=(0, 0, 0), width=2)  # make new line
+        else:  # for square function
+            table = [(x, y(x, task)) for x in arange(-15, 15, 0.001)]
+            for j in range(len(table))[:-2]:
+                dr.line([center[0] + table[j][0] * unit, center[1] + table[j][1] * unit,
+                         center[0] + table[j + 1][0] * unit, center[1] + table[j + 1][1] * unit],
+                        width=2, fill=(0, 0, 0))
+            dr.line([center[0] + table[-2][0] * unit, center[1] + table[-1][1] * unit,
+                     center[0] + table[-1][0] * unit, center[1] + table[-2][1] * unit],
+                    width=2, fill=(0, 0, 0))
+    im.save("line.png", "PNG")
+
+
 frame = Frame(root)  # make frame
 frame.pack(fill=X)
 
@@ -55,6 +95,10 @@ func.pack(side=LEFT, anchor=N)  # make textfield for x function
 build_btn = Button(frame, text="Build", width=7, font=string.font)
 build_btn.pack(side=RIGHT)
 build_btn.bind("<Button-1>", build)  # make build button
+
+build_btn = Button(frame, text="Export", width=7, font=string.font)
+build_btn.pack(side=RIGHT)
+build_btn.bind("<Button-1>", export)  # make export button
 
 clear_btn = Button(frame, text="Clear", width=7, font=string.font, command=lambda: c.delete("line"))
 clear_btn.pack(side=RIGHT)  # make clear button
